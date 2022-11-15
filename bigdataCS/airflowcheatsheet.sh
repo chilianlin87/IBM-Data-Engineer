a7986c8bdb98310487af6ae10375336ac58dd290
chilianlin87 <chi.lianlin87@gmail.com>
2022-11-15 17:22:46 -0500

    new update

--- bigdataCS/airflowcheatsheet.sh
+++ bigdataCS/airflowcheatsheet.sh
@@ -1,7 +1,9 @@
 
+export AIRFLOW_HOME=~/airflow
+airflow standalone #start airflow
 airflow dags list #list all dags
 airflow tasks list example_bash_operator #list tasks in a dag
 airflow dags unpause tutorial #unpause a dag
 airflow dags pause tutorial #pause a dag
-
+(sudo) cp my_first_dag.py $AIRFLOW_HOME/dags #submit the dag, copy python file to dags filder
 
