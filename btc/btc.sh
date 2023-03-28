fb49ac69c36fc1959881893f76a17f287e72f3e4
chilianlin87 <chi.lianlin87@gmail.com>
2023-03-27 22:54:33 -0400

    pickle

---
+++ btc/btc.sh
@@ -0,0 +1,7 @@
+bitcoin-qt #start bitcoin gui
+bitcoin-cli #bitcoin cli https://developer.bitcoin.org/reference/rpc/
+btc-rpc-explorer #start local btc explorer
+localhost:3002 #local host, btc-rpc-explorer
+bitcoind -daemon #start bitcoin daemon, start bitcoin core
+bitcoin-cli stop #stop bitcoin daemon
+
